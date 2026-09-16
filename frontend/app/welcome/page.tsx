b2c2e8f12308642b845bec3d438a53f44c51620a
Diego Figueroa Juarez <diego.undefined@gmail.com>
2026-09-16 15:13:53 -0500

    Create welcome page

--- frontend/app/welcome/page.tsx
+++ frontend/app/welcome/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-    return <div>Disfrute su comida :D</div>
+  return (
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl text-white">Bienvenido 👋</h1>
+      <h2 className="text-2xl text-gray-400 mt-4">
+        Por favor acercate a la puerta, tu mesa esta disponible
+      </h2>
+    </div>
+  );
 }
