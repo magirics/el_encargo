84aeaaa8dd51b82521aea14231c9ae05b850f2cf
Diego Figueroa Juarez <diego.undefined@gmail.com>
2026-09-16 15:16:49 -0500

    Fix next.config issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: ["192.168.1.38"],
+  allowedDevOrigins: ["192.168.1.*"],
   typescript: {
     ignoreBuildErrors: true,
   },
